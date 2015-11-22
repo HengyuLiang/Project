26a55db2221f0fd55560b23c2ad817ea6ab9e3df
harishjujjuru <harish.k.jujjuru@gmail.com>
2015-11-22 18:36:05 -0500

    Create Data.java

---
+++ CalendarProject/src/Calendar/Data.java
@@ -0,0 +1,27 @@
+package Calendar;
+
+public class Data {
+
+	private int year;
+	private int month;
+	
+	public int getYear(){
+		return year;
+	}
+	public void setYear(int year){
+		this.year=year;
+	}
+	
+	/*
+	 * Month value starts from 0 to 11.
+	 */
+	public int getMonth(){
+		return month;
+	}
+	
+	public void setMonth(){
+		this.month=month;
+	}
+	
+	
+}
