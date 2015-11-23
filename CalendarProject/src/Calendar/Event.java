df81e61bc32b73d035006df16fbc2043cdaf58ef
harishjujjuru <harish.k.jujjuru@gmail.com>
2015-11-22 19:22:38 -0500

    Create Event.java

---
+++ CalendarProject/src/Calendar/Event.java
@@ -0,0 +1,14 @@
+package Calendar;
+import java.util.Date;
+
+public class Event {
+	private String eventId;
+	private String eventName;
+	private String eventCreator;
+	private String eventDiscription;
+	private Date eventDate;
+	private String startTime, endTime;
+	
+ 
+ 
+}
