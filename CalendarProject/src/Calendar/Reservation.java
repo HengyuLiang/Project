208288dd756b468f597f9c99166aaa146c0ba1f0
harishjujjuru <harish.k.jujjuru@gmail.com>
2015-11-22 19:23:35 -0500

    Create Reservation.java

---
+++ CalendarProject/src/Calendar/Reservation.java
@@ -0,0 +1,11 @@
+package Calendar;
+import java.util.Date;
+public class Reservation 
+{
+	private String reservationId;
+	private String reservationName;
+	private String reservationResourcetype;
+	private String resourceLocation;
+	private Date resourceDate;
+	
+	}
