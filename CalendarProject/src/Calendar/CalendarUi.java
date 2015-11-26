8a81e086066e98b85f76031b6abafc16f19bbefb
ShuaiLu <slu6@stevens.edu>
2015-11-26 17:56:07 -0500

    creat some member of the CalendarUi and create an instructor of CalendarUi

--- CalendarProject/src/Calendar/CalendarUi.java
+++ CalendarProject/src/Calendar/CalendarUi.java
@@ -1,5 +1,28 @@
 package Calendar;
 
-public class CalendarUi {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
+public class CalendarUi extends JFrame{
+	JLabel lableDay[] = new JLabel[42];
+	JTextField showDate;
+	JButton weekName[] = new JButton[7];
+	String name[] = {"Sun","Mon","Tue","Wen","Thr","Fri","Sat"};
+	JButton nextMonth, previousMonth,nextYear,previousYear,save, event;
+	JTextArea message;
+	JMenuBar jmb;
+	JMenu File, Search;
+	JMenuItem Open,Save, Year, Month;
+	int year = 2015, month = 12;
+	CalendarData cd;
+	
+	public CalendarUi(){
+		
+	}
 }
