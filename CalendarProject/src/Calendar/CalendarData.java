d4258c6cde128537c7d48f60f0efba4572b287fc
HengyuLiang <lianghengyu3@gmail.com>
2015-11-26 17:42:48 -0500

    create a new method called setdate

--- CalendarProject/src/Calendar/CalendarData.java
+++ CalendarProject/src/Calendar/CalendarData.java
@@ -1,5 +1,7 @@
 package Calendar;
 
+import java.util.Calendar;
+
 public class CalendarData {
 
 	private int year;
@@ -14,7 +16,7 @@ public class CalendarData {
 	}
 	
 	/*
-	 * Month value starts from 0 to 11.
+	 * Month value starts from 1 to 12.
 	 */
 	public int getMonth(){
 		return month;
@@ -24,7 +26,26 @@ public class CalendarData {
 		this.month=month;
 	}
 	public String[] getDate(){
-
+		String temp[]=new String[42];
+		Calendar c=Calendar.getInstance();
+		c.set(year, month-1,1);
+		int week=c.get(Calendar.DAY_OF_WEEK)-1;
+		int day = 0;
+		if(month==1||month==3||month==5||month==7|| //set 31 days to the month 1,3,5,7,8,10,12
+		   month==8||month==10||month==12)
+			day=31;
+		if(month==4||month==6||month==9||month==11)  //set 30 days to the month 4,6,9,11
+			day=30;
+		if(month==2)  // if it is a leap year, then set 29days to February
+			if(((year%4==0)&&(year%100!=0))||(year%400==0))
+					day=29;
+			else      //if  it is not a leap year, then set 28 days to february
+				day=28;
+		for(int i=week,n=1;i<week+day;i++){
+			temp[i]=String.valueOf(n);
+			n++;
+		}
+		return temp;
 	}
 	
 }
