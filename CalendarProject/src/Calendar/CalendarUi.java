00f29687c6a5cac2386de3e0bf4ade6844fa34de
HengyuLiang <lianghengyu3@gmail.com>
2015-12-01 22:15:07 -0500

    add the window close function

--- CalendarProject/src/Calendar/CalendarUi.java
+++ CalendarProject/src/Calendar/CalendarUi.java
@@ -7,6 +7,8 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -187,6 +189,13 @@ public class CalendarUi extends JFrame implements ActionListener{
 		
 		setVisible(true);
 		
+		addWindowListener
+		(
+		 new WindowAdapter() {
+			 public void windowClosing(WindowEvent e) {
+				 System.exit(0);
+			 }
+		 } );
 		
 		
 	}
