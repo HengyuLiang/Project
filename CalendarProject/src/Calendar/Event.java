2834baf6c667dcf35da0095d1c6cbfebfde6da5f
harishjujjuru <harish.k.jujjuru@gmail.com>
2015-12-04 10:54:14 -0500

    add constructor and getters to events

--- CalendarProject/src/Calendar/Event.java
+++ CalendarProject/src/Calendar/Event.java
@@ -9,6 +9,38 @@ public class Event {
 	private String eventDiscription;
 	private Date eventDate;
 	private String startTime, endTime;
+	public Event(String eventId, String eventName, String eventCreator, String eventDiscription, Date eventDate,
+			String startTime, String endTime) {
+		super();
+		this.eventId = eventId;
+		this.eventName = eventName;
+		this.eventCreator = eventCreator;
+		this.eventDiscription = eventDiscription;
+		this.eventDate = eventDate;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	public String getEventId() {
+		return eventId;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public String getEventCreator() {
+		return eventCreator;
+	}
+	public String getEventDiscription() {
+		return eventDiscription;
+	}
+	public Date getEventDate() {
+		return eventDate;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public String getEndTime() {
+		return endTime;
+	}
 	
  
  
