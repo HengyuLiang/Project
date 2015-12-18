42ad815af79f5a3a0c7da09a01d77be596287709
harishjujjuru <harish.k.jujjuru@gmail.com>
2015-12-18 00:17:42 -0500

    refactor

--- CalendarProject/Calendar/CalendarEvent.java
+++ CalendarProject/Calendar/CalendarEvent.java
@@ -2,9 +2,7 @@ package Calendar;
 
 import java.util.Date;
 
-/**
- * Created by amk on 12/16/2015.
- */
+
 public interface CalendarEvent {
     public Long getId();
     public Date getEventDate();
